a812a734f0073f942270ec2aec39c930eff6c95e
Ismael SIDIBE-JOURDAN <isidibe-@e3r13p14.42.fr>
2018-07-10 13:08:24 +0200

    DSLR

--- scatter_plot.py
+++ scatter_plot.py
@@ -9,12 +9,23 @@ def main():
         # Keep only numerical values
     to_drop = list(df_train.select_dtypes(['object']).columns)
     to_drop.append('Index')
+    to_drop = to_drop[1:]
     df_train = df_train.drop(to_drop, axis=1)
     i = 1
+
+    Gryffindor = df_train[df_train['Hogwarts House'] == 'Gryffindor'].drop(['Hogwarts House'], axis=1)
+    Ravenclaw = df_train[df_train['Hogwarts House'] == 'Ravenclaw'].drop(['Hogwarts House'], axis=1)
+    Slytherin = df_train[df_train['Hogwarts House'] == 'Slytherin'].drop(['Hogwarts House'], axis=1 )
+    Hufflepuff = df_train[df_train['Hogwarts House'] == 'Hufflepuff'].drop(['Hogwarts House'], axis=1)
+    df_train = df_train.drop(['Hogwarts House'], axis=1)
+    print(Gryffindor.head())
     for feature in df_train:
         plt.subplot(4, 4, i)
         r = df_train[feature].sort_values()
-        plt.scatter(np.arange(len(r)), r.values, label='{}'.format(feature))
+        plt.scatter(np.arange(len(Gryffindor[feature].sort_values())), Gryffindor[feature].sort_values().values, label='{}'.format(feature), color='red', alpha=0.4)
+        plt.scatter(np.arange(len(Ravenclaw[feature].sort_values())), Ravenclaw[feature].sort_values().values, label='{}'.format(feature), color='blue', alpha=0.4)
+        plt.scatter(np.arange(len(Slytherin[feature].sort_values())), Slytherin[feature].sort_values().values, label='{}'.format(feature), color='green', alpha=0.4)
+        plt.scatter(np.arange(len(Hufflepuff[feature].sort_values())), Hufflepuff[feature].sort_values().values, label='{}'.format(feature), color='yellow', alpha=0.4)
         plt.title(feature)
         i += 1
     plt.show()
